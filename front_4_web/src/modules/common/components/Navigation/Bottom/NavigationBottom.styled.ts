a069197a4ac5418d98811a41b8ff8cd0afcf2b97
Dariusz (@sultanowski) <sultanowski.d@gmail.com>
2023-06-03 16:48:07 +0200

    style: Add NavigationBottom styles.

---
+++ front_4_web/src/modules/common/components/Navigation/Bottom/NavigationBottom.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledNavigationBottom = styled.nav`
+    position: fixed;
+    bottom: 0;
+    padding: 1rem;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
--- front_4_web/src/modules/common/components/Navigation/Top/NavigationTop.styled.ts
+++ front_4_web/src/modules/common/components/Navigation/Top/NavigationTop.styled.ts
@@ -4,9 +4,17 @@ export const StyledNavigationTop = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    padding: 1rem;
 `
 
 export const StyledDiv = styled.div`
     display: flex;
     gap: .5rem;
+`
+
+export const NavigationProfile = styled.div`
+    width: 32px;
+    height: 32px;
+    border-radius: 100%;
+    background-color: black;
 `
