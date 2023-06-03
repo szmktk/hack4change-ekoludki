002b376f6ad1c1a19b83d97961e8b368a4a0295c
Dariusz (@sultanowski) <sultanowski.d@gmail.com>
2023-06-03 17:41:08 +0200

    fix: Leaflet implementation.

--- front_4_web/src/modules/dashboard/components/Content.tsx
+++ front_4_web/src/modules/dashboard/components/Content.tsx
@@ -1,9 +1,7 @@
-import GlobalStyles from "../../common/styles/GlobalStyles";
 import { Map } from "../../details/components/Map";
 export const Content = (): JSX.Element => {
     return(
       <>
-        <GlobalStyles />
         <Map/>     
       </>
     )
--- front_4_web/src/modules/dashboard/components/MainViewer.tsx
+++ front_4_web/src/modules/dashboard/components/MainViewer.tsx
@@ -1,16 +1,14 @@
-
 import GlobalStyles from "../../common/styles/GlobalStyles"
 import { NavigationTop } from "../../common/components/Navigation/Top/NavigationTop"
 import { NavigationBottom } from "../../common/components/Navigation/Bottom/NavigationBottom"
-import { Content } from "./Content"
-
+import { Map } from "../../details/components/Map"
 export const MainViewer = () => {
     return (
         <>
             <GlobalStyles />
             <div>
                 <NavigationTop />
-                <Content />
+                <Map />
                 <NavigationBottom />
             </div>
         </>
