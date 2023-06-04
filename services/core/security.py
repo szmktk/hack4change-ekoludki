73f0d45f07c086f91f04a06a05184219ab38cd21
Bartlomiej M. Kraus <bartmkraus@gmail.com>
2023-06-04 12:20:15 +0200

    Security to work on futher

--- services/core/security.py
+++ services/core/security.py
@@ -1,9 +1,11 @@
 # https://fastapi-users.github.io/fastapi-users/11.0/configuration/databases/sqlalchemy/
 #TODO https://fastapi-users.github.io/fastapi-users/11.0/configuration/authentication/backend/
 
-from typing import AsyncGenerator
+import uuid
+from typing import AsyncGenerator, Optional
 
-from fastapi import Depends
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, UUIDIDMixin
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyAccessTokenDatabase,
@@ -12,11 +14,15 @@ from fastapi_users_db_sqlalchemy.access_token import (
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-from fastapi_users.authentication import CookieTransport
+from fastapi_users.authentication import CookieTransport, AuthenticationBackend, BearerTransport, JWTStrategy
+
+from .db import User, get_user_db
 # cookie_transport = CookieTransport(cookie_max_age=3600)
 
 DATABASE_URL = "sqlite+aiosqlite:///./main.db"
+SECRET = "SECRET"
 
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 class Base(DeclarativeBase):
     pass
@@ -50,3 +56,36 @@ async def get_access_token_db(
     session: AsyncSession = Depends(get_async_session),
 ):  
     yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy,
+)    
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user: User, request: Optional[Request] = None):
+        print(f"User {user.id} has registered.")
+
+    async def on_after_forgot_password(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"User {user.id} has forgot their password. Reset token: {token}")
+
+    async def on_after_request_verify(
+        self, user: User, token: str, request: Optional[Request] = None
+    ):
+        print(f"Verification requested for user {user.id}. Verification token: {token}")
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
